9e296bf550a6eb18020afb396393f747eedb6a87
unknown <nghiahqt@gmail.com>
2021-04-01 16:00:02 +0700

    bai7-2

---
+++ 7.Bien_Kieu_Du_Lieu_Va_Toan_Tu/Thuc_hanh/Su_Dung_Toan_Tu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputheight = prompt('Nhập chiều rộng');
+inputwidth = prompt('Nhập chiều dài');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width*height
+document.write("Diện tích hình chữ nhật là :" + area );
